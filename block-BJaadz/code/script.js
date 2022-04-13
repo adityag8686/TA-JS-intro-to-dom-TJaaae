b3ef5e5d1c836e5061e3a5a5796ff007364ec8f7
aditya <agoswami0708g@gmail.com>
2022-04-13 14:04:07 +0530

    done

--- block-BJaadz/code/script.js
+++ block-BJaadz/code/script.js
@@ -1,11 +1,11 @@
 // Select the h1 element and store it in a variable named heading.
-
+let heading = document.querySelector('h1');
 // Check the typeof heading and log it.
-
+typeof heading
 // Change the color of heading to black.
-
+heading.style.color = "black"
 // Select all the hr elements and store it in a variable named allHrs using querySelectorAll
-
+let allHrs = document.querySelectorAll("hr");
 /* 
 Convert the NodeList returned by querySelectorAll to Array using Array.from() or spread operator and store it in allHrsArray
 
@@ -14,15 +14,15 @@ Array.from converts an array kind of data into array so we can use methods like 
 HINT:
 let allHrsArray = Array.from(allHrs)
 */
-
+let allHrsArray = Array.from(allHrs);
 // Set the border of the all the hr elements to "1px solid tomato"
-
+allHrsArray.forEach((b) => b.style.border = "1px solid tomato")
 // Change the background of all the hr to "antiquewhite" using for of loop.
-
+allHrsArray.forEach((b) => b.style.backgroundColor = "antiquewhite")
 // Change the 'border-radius' of all the hr to "5px" using array.
-
+allHrsArray.forEach((b) => b.style.borderRadius = "5px")
 // Change the alignment of the heading(h1) to center.
-
+heading.style.alignContent = "center"
 // Change the font size of the heading to 3rem.
 
 // Change the border of hr with class 'image' to `2px solid purple`.
